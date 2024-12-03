81ed9cf05e5bb13ceb360051ce95ef82b2424dc0
Maartje <brouwer.mm@gmail.com>
2024-12-03 15:05:57 +0100

    Create script.py

---
+++ advent_of_code/2024/script.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Advent of Code 2024
+"""
+
+#%% Day 1
+"""
+Pair up the numbers and measure how far apart they are. Pair up the smallest 
+number in the left list with the smallest number in the right list, then the 
+second-smallest left number with the second-smallest right number, and so on.
+
+Within each pair, figure out how far apart the two numbers are; you'll need to 
+add up all of those distances.
+"""
+
+
