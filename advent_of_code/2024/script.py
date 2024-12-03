b2168c90c917b093016b4cdf247a7c835e0ac57d
Maartje <78266071+maartje-git@users.noreply.github.com>
2024-12-03 16:47:22 +0100

    Update script.py

--- advent_of_code/2024/script.py
+++ advent_of_code/2024/script.py
@@ -12,5 +12,19 @@ second-smallest left number with the second-smallest right number, and so on.
 Within each pair, figure out how far apart the two numbers are; you'll need to 
 add up all of those distances.
 """
+#make 2 empty lists
+list_1 = []
+list_2 = []
+
+#open data and loop through lines.
+with open('advent_of_code/2024/data_day1.txt') as data:
+    for line in data:
+        #split line in 2 separate numbers
+        number_1, number_2 = line.split('   ‘,1)
+        #append numbers to their appropriate list
+        list_1.append(int(number_1))
+        list_2.append(int(number_2))
+
+
 
 
